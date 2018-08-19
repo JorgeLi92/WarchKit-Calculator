bdd85096318eac5887b20035e7319764db22f656
Jorge Elias Blanco Santonja <jorgeeliasblancosantonja@macbook-pro-2.local>
2018-08-19 18:59:25 +0200

    Initial Commit

---
+++ Calculadora_WarchKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculadora_WarchKit
+//
+//  Created by Jorge Elias Blanco Santonja on 19/8/18.
+//  Copyright © 2018 Jorge Elias Blanco Santonja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
